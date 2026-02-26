f3146ed079c029bd3d8642b1ce61179ab62b9e38
Mariana Lima <marianaml0308@gmail.com>
2026-02-26 20:21:21 -0300

    Atualização

--- student/src/main/java/com/mml/student/services/EstudanteService.java
+++ student/src/main/java/com/mml/student/services/EstudanteService.java
@@ -3,10 +3,11 @@ package com.mml.student.services;
 import com.mml.student.models.EstudanteModel;
 import com.mml.student.repositories.EstudanteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class EstudanteService {
     @Autowired
     private EstudanteRepository estudanteRepository;
